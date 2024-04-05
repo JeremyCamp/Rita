6e1a5e12815aeb63024b53ab078342a05a385e9e
Jeremy <jeremy@MacBook-Pro-de-Jeremy.local>
2024-04-05 10:15:02 -0400

    correction chemin css

--- Front/JS/app.js
+++ Front/JS/app.js
@@ -1,20 +1,26 @@
 const users = "./Front/JSON/users.json";
 
 fetch(users)
-  .then(response => response.json())
-  .then(data => {
-    const btnConnect = document.querySelector(".btnConnect");
-    btnConnect.addEventListener("click", ()=> {
-        const user = document.querySelector(".user");
-        const password = document.querySelector(".password");
-        if(user.value == data.users[0].name && password.value == data.users[0].password){
-            window.open("./menu.html")
-        }
-        else {
-            alert("Veuillez verifier vos information de connection");
-        }
-    })
+.then(response => response.json())
+.then(data => {
+  const btnConnect = document.querySelector(".btnConnect");
+  btnConnect.addEventListener("click", ()=> {
+    const user = document.querySelector(".user");
+    const password = document.querySelector(".password");
+    let userFound = false;
+    for(let i = 0; i < data.users.length; i++) {
+      if(user.value === data.users[i].name && password.value === data.users[i].password){
+          userFound = true;
+          break;
+      }
+    }
+    if(userFound){
+        window.open("./menu.html", "_self");
+    } else {
+        alert("Veuillez vérifier vos informations de connexion");
+    }
   })
-  .catch(error => {
-    console.error('Une erreur s\'est produite :', error);
-  });
+})
+.catch(error => {
+  console.error('Une erreur s\'est produite :', error);
+});
